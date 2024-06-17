f61176e7d4fe815190bf966b2e065fa31f04b514
tituzzo <tito.barbieri@studio.unibo.it>
2024-06-17 16:48:47 +0200

    t

--- build/CMakeFiles/flock_simulation.dir/bird.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/flock_simulation.dir/bird.cpp.o: \
- /home/tito/Desktop/Cavina_Barbieri/bird.cpp /usr/include/stdc-predef.h \
- /home/tito/Desktop/Cavina_Barbieri/bird.hpp \
- /home/tito/Desktop/Cavina_Barbieri/vector2d.hpp
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tito/Desktop/Cavina_Barbieri
-# Build directory: /home/tito/Desktop/Cavina_Barbieri/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test.t "/home/tito/Desktop/Cavina_Barbieri/build/test.t")
-set_tests_properties(test.t PROPERTIES  _BACKTRACE_TRIPLES "/home/tito/Desktop/Cavina_Barbieri/CMakeLists.txt;35;add_test;/home/tito/Desktop/Cavina_Barbieri/CMakeLists.txt;0;")
-subdirs("third_party/sfml-widgets")
